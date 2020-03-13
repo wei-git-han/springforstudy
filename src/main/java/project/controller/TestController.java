b1a13c651b62e345f3709b8b36af0c49229b3a38
wei-git-han <18303010235@163.com>
2020-03-13 22:46:52 +0800

    springboot&mybatis

--- src/main/java/project/controller/TestController.java
+++ src/main/java/project/controller/TestController.java
@@ -1,11 +1,13 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.entity.User;
+import project.service.serviceImpl.UserService;
 
 import javax.annotation.Resource;
 import java.sql.ResultSet;
@@ -15,21 +17,12 @@ import java.util.List;
 @Controller
 public class TestController {
 
-    @Resource
-    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/userlist")
     public String getUserList(ModelMap map){
-        String sql = "select * from user_han";
-        List<User> userList = jdbcTemplate.query(sql,new RowMapper<User>(){
-            User user = null;
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException{
-                user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setDeptId(rs.getString("deptId"));
-                return user;
-            }});
+        List<User> userList = userService.getList("11");
         for (User user:userList){
             System.out.println(user.getName());
         }
