391cd14a8597f50ee9321cdd865632d0cae69031
wei-git-han <18303010235@163.com>
2020-03-17 22:22:24 +0800

    从dao层直接写SQL

--- src/main/java/project/dao/UserDao.java
+++ src/main/java/project/dao/UserDao.java
@@ -1,6 +1,7 @@
 package project.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import project.entity.User;
 
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 @Mapper
 public interface UserDao {
+    @Select("select * from user_han where id = #{id}")
     List<User> getList(String id);
 }
