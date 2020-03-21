3c920f85959077f3c4771aaebab99c50958a1426
wei-git-han <18303010235@163.com>
2020-03-21 09:14:17 +0800

    springboot整合mybatis

--- src/main/java/com/example/han/springtest/SpringtestApplication.java
+++ src/main/java/com/example/han/springtest/SpringtestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@MapperScan("com.example.han.springtest")
 public class SpringtestApplication {
 
     public static void main(String[] args) {
